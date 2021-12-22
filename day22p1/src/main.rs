6cd79c57b82bb687f88ae5c62cfde83d694cffac
Benno Rice <benno@jeamland.net>
2021-12-22 16:26:57 +1100

    Day 22, part 1.

---
+++ day22p1/src/main.rs
@@ -0,0 +1,67 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use itertools::Itertools;
+
+fn main() {
+    let file = File::open(std::env::args_os().nth(1).unwrap()).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut cubes = HashSet::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let (state, ranges) = line.split_whitespace().collect_tuple().unwrap();
+        let (x_range, y_range, z_range) = if let Some(r) = ranges
+            .split_terminator(',')
+            .filter_map(|r| {
+                let (start, end) = r
+                    .split_at(2)
+                    .1
+                    .split_terminator("..")
+                    .collect_tuple()
+                    .unwrap();
+                let mut start = start.parse::<isize>().unwrap();
+                let mut end = end.parse::<isize>().unwrap();
+
+                if (start > 50 && end > 50) || (start < -50 && end < -50) {
+                    return None;
+                }
+
+                if start < -50 {
+                    start = -50;
+                } else if start > 50 {
+                    start = 50;
+                }
+
+                if end < -50 {
+                    end = -50;
+                } else if end > 50 {
+                    end = 50;
+                }
+
+                Some(start..=end)
+            })
+            .collect_tuple()
+        {
+            r
+        } else {
+            continue;
+        };
+
+        for x in x_range.clone() {
+            for y in y_range.clone() {
+                for z in z_range.clone() {
+                    if state == "on" {
+                        cubes.insert((x, y, z));
+                    } else {
+                        cubes.remove(&(x, y, z));
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{} cubes", cubes.len());
+}
